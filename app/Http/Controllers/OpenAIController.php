bea2be0d11b8e70c349ec18c7526dbb603e91dfd
Athira <athira.ks@infolitz.com>
2023-07-04 14:42:04 +0530

    data send to mail

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Message;
 
 use GuzzleHttp\Client;
 class OpenAIController extends Controller
@@ -15,23 +17,43 @@ class OpenAIController extends Controller
      * @return response(request )
      */
 
-     public function index()
-     {
-        
-        return view('chatWindow');
-     }
+    public function index()
+    {
+    
+    return view('chatWindow');
+    }
+
+    // public function botData(Request $request): JsonResponse
+    // {
+    // // Your existing code...
+
+    // // Get the content from the response
+    // $content = $data['choices'][0]['message']['content'];
+
+    // // Send the content as an email
+    // Mail::send([], [], function (Message $message) use ($content) {
+    //     $message->to('athirachinu378@gmail.com') // Set the recipient email address
+    //         ->subject('Bot Data') // Set the email subject
+    //         ->setBody($content); // Set the email body as the content
+    // });
+
+    // // Return the response as JSON
+    // return response()->json($data['choices'][0]['message'], 200, [], JSON_PRETTY_PRINT);
+    // }
+
+
     public function botData(Request $request): JsonResponse
     {
        
        
-$search = $request->user_input;
- $client = new Client([
-  'curl' => [
-      CURLOPT_CAINFO => base_path('resources/assets/cacert.pem')
-  ]
-]);
+    $search = $request->user_input;
+    $client = new Client([
+    'curl' => [
+        CURLOPT_CAINFO => base_path('resources/assets/cacert.pem')
+    ]
+    ]);
 
-$response = $client->post("https://api.openai.com/v1/chat/completions", [
+    $response = $client->post("https://api.openai.com/v1/chat/completions", [
     'headers' => [
         'Content-Type' => 'application/json',
         'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
@@ -51,19 +73,34 @@ $response = $client->post("https://api.openai.com/v1/chat/completions", [
         "presence_penalty" => 0.5,
         "stop" => ["11."],
     ]
-]);
+    ]);
 
-$data = json_decode($response->getBody(), true);
-$contant = $data['choices'][0]['message']['content'];
-// $modifiedString = str_replace($wordToFind, $wordToFind . " " . $stringToInsert, $originalString);
-// $modifiedString = str_replace($wordToFind, $wordToFind . " " . $stringToInsert, $originalString);
-// substr_replace($oldstr, $str_to_insert, $pos, 0);
-$contant = str_replace('```'," ",$contant);
+    $data = json_decode($response->getBody(), true);
+    $contant = $data['choices'][0]['message']['content'];
+    // $modifiedString = str_replace($wordToFind, $wordToFind . " " . $stringToInsert, $originalString);
+    // $modifiedString = str_replace($wordToFind, $wordToFind . " " . $stringToInsert, $originalString);
+    // substr_replace($oldstr, $str_to_insert, $pos, 0);
+    $contant = str_replace('```'," ",$contant);
 
 
 
-$data['choices'][0]['message']['content'] = nl2br($contant);
+    $data['choices'][0]['message']['content'] = nl2br($contant);
 
-return response()->json($data['choices'][0]['message'], 200, array(), JSON_PRETTY_PRINT);
-     }
+    $content = $data['choices'][0]['message']['content'];
+
+    // Prepare the email data
+    $to = 'athirachinu378@gmail.com'; // Set the recipient email address
+    $subject = 'Bot Data'; // Set the email subject
+    $body = $content; // Set the email body as the content
+
+    // Send the email
+    Mail::raw($body, function (Message $message) use ($to, $subject) {
+        $message->to($to)
+            ->subject($subject);
+    });
+
+
+    return response()->json($data['choices'][0]['message'], 200, [], JSON_PRETTY_PRINT);
+    }
+        
 }
---
+++ resources/views/emails/bot_data.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bot Data</title>
+</head>
+<body>
+    <h1>Bot Data</h1>
+    <p>{{ $content }}</p>
+</body>
+</html>

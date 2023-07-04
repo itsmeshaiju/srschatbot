06f0b22eb00e6b378647104d0e1ace68a670b9f9
Anshad Ek <anshadali.ek@infolitz>
2023-07-04 14:38:53 +0530

    add login register and chat data  to db

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OpenAIController;
+use App\Http\Controllers\Auth\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,13 @@ use App\Http\Controllers\OpenAIController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-
-Route::get('/', [OpenAIController::class, 'index']);
+//Auth
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('dashboard', [AuthController::class, 'dashboard']); 
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+//Open ai
+Route::get('/', [OpenAIController::class, 'index'])->name('index');
 Route::post('chat-with-bot', [OpenAIController::class, 'botData'])->name('chat.with.bot');

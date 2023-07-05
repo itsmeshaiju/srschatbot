3c5f9fbd906dc162f0e438daae3a5cb5d70e7cd3
Shaiju PM <shaiju.pm@infolitz.com>
2023-07-05 08:40:57 +0530

    question field changed to text from string

--- database/migrations/2023_07_04_150122_create_questions_table.php
+++ database/migrations/2023_07_04_150122_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('cat_id')->constrained()->onDelete('cascade');
-            $table->string('question_name',250);
+            $table->text('question_name');
             $table->boolean('status')->default(1)->comment('1:enabled,0:disabled');
             $table->timestamps();
         });

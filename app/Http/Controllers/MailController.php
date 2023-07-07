2e7aa008414572218ae8319ffc38f81929e0aed9
Athira <athira.ks@infolitz.com>
2023-07-07 12:04:18 +0530

    mail controller subject

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
 // use Illuminate\Http\Request;
@@ -10,24 +9,21 @@ class MailController extends Controller
 {
 
     public function sendMail($pdfName, $toMail = false)
-{
-    
-
-    $recipient = auth::user()->email; // Set the recipient email address
-    $subject = 'SRS Document'; // Set the email subject
-    $filePath = $pdfName; // Set the filepath for the PDF attachment
-
-    Mail::raw('Hai ' . Auth::user()->name . ', Please find the attached SRS Document along with the mail. Have a nice day !' , function ($message) use ($recipient, $subject, $filePath, $pdfName) //The Mail::raw function sends the email.
     {
-        $message->to($recipient) // Specify the recipient's email
-            ->subject($subject) // Specify the email subject 
-            ->attach($filePath, ['as' => $pdfName]); // Specify the file path of the PDF attachment
-    });
-    
+        $recipient = auth::user()->email; // Set the recipient email address
+        $subject = 'SRS Document'; // Set the email subject
+        $filePath = $pdfName; // Set the filepath for the PDF attachment
 
-    return 'Email sent with PDF attachment.';
-}
+        Mail::raw('Hai ' . Auth::user()->name . ', Please find the attached SRS Document along with the mail. Have a nice day !'  , function ($message) use ($recipient, $subject, $filePath, $pdfName) //The Mail::raw function sends the email.
+        {
+            $message->to($recipient) // Specify the recipient's email
+                ->subject($subject) // Specify the email subject 
+                ->attach($filePath, ['as' => $pdfName]); // Specify the file path of the PDF attachment
+        });
+        
 
+        return 'Email sent with PDF attachment.';
+    }
 
 }
 
--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,28 +2,24 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Http\Request;
-// use App\Models;
 use TCPDF;
-// use Dompdf\Dompdf;
-// use App\Http\Controllers\OpenAIController;
 use App\Models\gptQuestionAnswer;
 class PdfController extends Controller
 {
-    public function generatePDF($pdfName)
+    public function generatePDF($pdfName) //The generatePDF function takes a parameter $pdfName (the name of the PDF file)
     {
-        $data = gptQuestionAnswer::select('id', 'question_and_answer')->where('user_id',auth()->user()->id)->orderBy('id','desc')->first();
-        $data = json_decode($data['question_and_answer'], TRUE);
-        $content = $data['answer'];
+        $data = gptQuestionAnswer::select('id', 'question_and_answer')->where('user_id',auth()->user()->id)->orderBy('id','desc')->first(); //It retrieves the necessary data from the gptQuestionAnswer table using the select, where, and orderBy methods. 
+        $data = json_decode($data['question_and_answer'], TRUE); //The retrieved data is then decoded from JSON format and assigned to the $data variable.
+        $content = $data['answer']; //The $content variable stores the answer value from the decoded data.
     
         // Generate PDF using TCPDF
-        $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false);
-        $pdf->SetMargins(15, 15, 15);
-        $pdf->AddPage();
-        $pdf->SetFont('helvetica', '', 12);
-        $pdf->MultiCell(0, 10, 'Content: '.$content, 0, 1);
-        $pdfContent = public_path('SRSDoc/' . $pdfName);
-        $pdf->Output($pdfContent, 'F');
+        $pdf = new TCPDF('P', 'mm', 'A4', true, 'UTF-8', false); // Set page orientation, unit of measurement, and page size
+        $pdf->SetMargins(15, 15, 15); //Set margins
+        $pdf->AddPage(); // Add new page
+        $pdf->SetFont('helvetica', '', 12); //The font is set to 'Helvetica' with a size of 12
+        $pdf->MultiCell(0, 10, 'Content: '.$content, 0, 1); //Multicell method is used to add the content to the pdf and displayed as a multi-line cell
+        $pdfContent = public_path('SRSDoc/' . $pdfName); //The $pdfContent variable is assigned the path where the PDF file will be saved.
+        $pdf->Output($pdfContent, 'F'); //used to save the PDF file to the specified path
 
         
         return $pdfContent;

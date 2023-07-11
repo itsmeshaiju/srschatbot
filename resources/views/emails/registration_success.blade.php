5e740d790c944e766f03b974ab64890a157affb9
Athira <athira.ks@infolitz.com>
2023-07-11 09:40:56 +0530

    registration success message

---
+++ resources/views/emails/registration_success.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Successful</title>
+</head>
+<body>
+    <h1>Registration Successful</h1>
+    <p>Dear {{ $user->name }},</p>
+    <p>Your registration process has been completed successfully.</p>
+    <!-- Add any additional information or styling here -->
+</body>
+</html>
